7bd2062d53679a4806dafed8c29a17d09f603660
Eliise S <elseling@microsoft.com>
2020-07-27 12:58:14 +0000

    Add timeout

--- scripts/wait-for-bridge.sh
+++ scripts/wait-for-bridge.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
+SECONDS=0
+
 echo "Waiting for the bridge to be up"
 CRDS=$(kubectl get crd --all-namespaces | wc -l)
 
 while [ "$CRDS" -eq "0" ] || [ "$CRDS" != "$NEW_CRDS" ]; do 
+    if (( $SECONDS > 180))
+        then 
+        echo "Terminating due to timeout; This operation has taken $SECONDS seconds"
+        exit 1
+    fi
+
     CRDS=$(kubectl get crd --all-namespaces | wc -l)
     echo "There are $CRDS crds"
     echo "Sleeping for 10 seconds"
